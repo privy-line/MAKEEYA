05ba65f008625da8f0a1d4ba35573fd766e7d40a
Adelice <nancytoniw@gmail.com>
2019-06-14 17:35:46 +0200

    commit

--- MAKEEYA/urls.py
+++ MAKEEYA/urls.py
@@ -20,4 +20,7 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('makeeya.urls')),
+    url(r'^logout/$',views.logout, {"next_page": '/'}),
+     url(r'^accounts/', include('registration.backends.simple.urls')),
+    
 ]
--- makeeya/admin.py
+++ makeeya/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item,Seller,Category
+
+
+admin.site.register(Item)
+admin.site.register(Seller)
+admin.site.register(Category)
+
