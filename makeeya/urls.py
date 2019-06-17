f9800440e51a503eb869a7023b1d74d8fd45ee52
Pnshimiye <pnshimiye@gmail.com>
2019-06-17 12:51:43 +0200

    add create_item url

--- makeeya/urls.py
+++ makeeya/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
  
     url(r'^profile/(\d+)',views.profile,name ='profile'),
     url(r'^request/$', views.post_request, name='post_request')
+    url (r'^create_item/$', views.create_item, name='create_item')
 ]
